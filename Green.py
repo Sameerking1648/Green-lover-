a589b6023cac481bc55317d152178641b447c048
Sameerking1648 <107364095+Sameerking1648@users.noreply.github.com>
2022-09-04 08:40:54 +0900

    Update Green.py

--- Green.py
+++ Green.py
@@ -1,30 +1,9 @@
-import os, platform, time
-os.system('termux-setup-storage')
-try:
-	import requests
-except ImportError:
-	print('\n [×] requests module not installed!...\n')
-	os.system('pip install requests')
+import os
 
-try:
-	import concurrent.futures
-except ImportError:
-	print('\n [×] Futures module not installed!...\n')
-	os.system('pip install futures')  
-try:
-	import bs4
-except ImportError:
-	print('\n [×] Bs4 module not installed!...\n')
-	os.system('pip install bs4')
-print('\n\x1b[1;37m[•] Checking Update...');time.sleep(0.5)
 os.system('git pull')
-def Run():
-        bit = platform.architecture()[0]
-        if bit == '64bit':
-            print("\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
-            print('[•] Join Over Facebook Group First')
-            os.system('xdg-open https://t.me/Sameerking1648')
-            import GREEN
-        else:
-            exit('\033[1;31m[×] Sorry, Your Device Not Support This Tool')
-Run()
+
+os.system('termux-setup-storage')
+
+import enc_enc_green
+
+enc_enc_green.login()
